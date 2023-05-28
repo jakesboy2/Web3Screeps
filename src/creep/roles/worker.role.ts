0395015259974235891073f662650c5694cedd36
Jacob <jakeywaldrip@gmail.com>
2023-05-28 16:11:25 -0500

    Added basics for worker

--- src/creep/roles/worker.role.ts
+++ src/creep/roles/worker.role.ts
@@ -3,5 +3,15 @@ import { ICreepRole } from "./types";
 export const WorkerRole: ICreepRole = {
   runCreep: (creep: Creep) => {
     console.log("Running worker", creep.name);
+  },
+
+  getBody: () => [WORK, CARRY, CARRY, MOVE, MOVE],
+
+  getOptions: (homeRoom: string) => {
+    return {
+      memory: {
+        role: "worker", homeRoom, working: false
+      }
+    };
   }
 }
