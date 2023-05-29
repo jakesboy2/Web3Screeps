b8027f16e3d00f91ab91247ca6b8150198661d46
Jacob <jakeywaldrip@gmail.com>
2023-05-29 02:22:55 -0500

    wrote worker logic

--- src/creep/roles/worker.role.ts
+++ src/creep/roles/worker.role.ts
@@ -1,8 +1,60 @@
-import { ICreepRole } from "./types";
+import { CreepRoles, ICreepRole } from "./types";
+
+const getEnergySource = (creep: Creep): Resource<ResourceConstant> | null => {
+  return creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {
+    filter: (resource) => {
+      return resource.resourceType === RESOURCE_ENERGY;
+    }
+  });
+};
+
+const getEnergyTarget = (creep: Creep) => {
+  const energy = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {
+    filter: (resource) => {
+      return resource.resourceType === RESOURCE_ENERGY;
+    }
+  });
+
+  return energy;
+};
+
+const computeCreepWorkingState = (creep: Creep) => {
+  if(!creep.memory.working && creep.store.getFreeCapacity() === 0) {
+    creep.memory.working = true;
+  }
+
+  if(creep.memory.working && creep.store.getUsedCapacity() === 0) {
+    creep.memory.working = false;
+  }
+};
 
 export const WorkerRole: ICreepRole = {
   runCreep: (creep: Creep) => {
-    console.log("Running worker", creep.name);
+    computeCreepWorkingState(creep);
+
+    if(creep.memory.working) {
+      const controller = creep.room.controller;
+      if(!controller) return;
+
+      if(creep.pos.inRangeTo(controller, 3)) {
+        creep.upgradeController(controller);
+      }
+      else {
+        creep.moveTo(controller);
+      }
+    }
+
+    if(!creep.memory.working) {
+      const target = getEnergyTarget(creep);
+      if(!target) return;
+
+      if(creep.pos.isNearTo(target)) {
+        creep.pickup(target);
+      }
+      else {
+        creep.moveTo(target);
+      }
+    }
   },
 
   getBody: () => [WORK, CARRY, CARRY, MOVE, MOVE],
@@ -10,7 +62,7 @@ export const WorkerRole: ICreepRole = {
   getOptions: (homeRoom: string) => {
     return {
       memory: {
-        role: "worker", homeRoom, working: false
+        role: CreepRoles.Worker, homeRoom, working: false
       }
     };
   }
--- src/creep/types.ts
+++ src/creep/types.ts
@@ -3,11 +3,13 @@ import { BoostrapperRole } from "./roles/bootstrapper.role";
 import { MinerRole } from "./roles/miner.role";
 import { CreepRoles, ICreepRole } from "./roles/types";
 import { WorkerRole } from "./roles/worker.role";
+import { CarrierRole } from "./roles/carrier.role";
 
 export const CreepStrategyMap: {[role in CreepRoles]: ICreepRole} = {
   bootstrapper: BoostrapperRole,
   miner: MinerRole,
   worker: WorkerRole,
+  carrier: CarrierRole,
 };
 
 export const getRoleStrategy = (role: CreepRoles) => {
